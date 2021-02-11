5fc2828ded4fe42eacbd4105aa8c365fa78c6442
thelonggoodbuy <thelonggoodbuy1991@gmail.com>
2021-02-11 12:17:32 +0200

    11_02_2021

--- users/admin.py
+++ users/admin.py
@@ -1,14 +1,1 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-
-
-from .forms import CustomUserCreationForm
-from .models import CustomUser
-
-
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserCreationForm
-    model = CustomUser
-
-admin.site.register(CustomUser)
--- users/tests.py
+++ users/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, SimpleTestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-
+from .models import CustomUser
 
 class HomePageTest(SimpleTestCase):
 
@@ -16,14 +16,12 @@ class HomePageTest(SimpleTestCase):
 
     def test_view_correct_template(self):
         response = self.client.get(reverse ('home'))
-        self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
 
 
-class SignupPageTest(TestCase):
+class SignUpPageTest(TestCase):
     
-    username = 'newuser'
-    email = 'newuser@email.com'
+    email = 'newuser@gmail.com'
 
     def test_signup_page_status_code(self):
         response = self.client.get('/users/signup/')
@@ -38,8 +36,7 @@ class SignupPageTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'signup.html')
 
-    def test_signup_form(self):
-        new_user = get_user_model().objects.create_user(self.username, self.email)
+    def test_creating_user_in_database(self):
+        new_user = get_user_model().objects.create_user(self.email)
         self.assertEqual(get_user_model().objects.all().count(),1)
-        self.assertEqual(get_user_model().objects.all()[0].username, self.username)
         self.assertEqual(get_user_model().objects.all()[0].email, self.email)
