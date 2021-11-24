5328d99b8694d5f4a87d2cff7ffc87aae6c217b8
thelonggoodbuy <thelonggoodbuy1991@gmail.com>
2021-11-24 10:43:07 +0200

    Task4.I had added: list of cinemas, admins permission to add cinemas data.Trash is cleared

--- shows/urls.py
+++ shows/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.contrib.auth.decorators import user_passes_test
 
 from .views import CinemaListView, CinemaCreateView, CinemaUpdateView, CinemaDetailView, CinemaDeleteView 
 
